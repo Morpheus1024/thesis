43ed5e7692040feec02218855ae887335bd37d18
Mikołaj Galant <mikolaj.galant@gmail.com>
2024-03-22 12:54:47 +0100

    dodanie todosow do programu; zapis przykladowych zdjęc

--- inz.py
+++ inz.py
@@ -1,27 +1,103 @@
+# Stages of creating a semantic 3d map of the environment
+
+# 1. data acquisition
+# - [x] camera images
+# - [x] depth map
+
+# 2. preprocessing
+# - [ ] calibration - compensation for geometric error of sensor spacing
+# - [ ] image segmentation
+# - [ ] depth map filtering
+
+# 3. 3D reconstruction
+# - ] cloud points from depth map
+# - ] 3D mesh - a mesh representing surfaces is created from the point cloud
+
+# 4. semantic segmentation
+# - [ ] classification of each segment
+# - [ ] assignment of labels to objects - apply multiple models
+
+# 5. integration
+# - [ ] combine data from segmentation, classification and 3D mesh to create a semantic map
+# - [ ] visialization of data
+
+
 import pyrealsense2 as rs
 import numpy as np
 import cv2
 
-def sprawdzenie_sprzetu():
-    # Configure depth and color streams
-    pipeline = rs.pipeline()
-    config = rs.config()
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
 
-    # Get device product line for setting a supporting resolution
-    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-    pipeline_profile = config.resolve(pipeline_wrapper)
-    device = pipeline_profile.get_device()
-    device_product_line = str(device.get_info(rs.camera_info.product_line))
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
 
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
 
-    found_rgb = False
-    for s in device.sensors:
-        if s.get_info(rs.camera_info.name) == 'RGB Camera':
-            found_rgb = True
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+pipeline.start(config)
+
+try:
+
+    while True:
+
+        ## 1. Acquire image and depth data from camera
+
+        # Wait for a coherent pair of frames: depth and color
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
+
+        # If depth and color resolutions are different, resize color image to match depth image for display
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            images = np.hstack((resized_color_image, depth_colormap))
+        else:
+            images = np.hstack((color_image, depth_colormap))
+
+        ## 2. Preprocessing 
+
+        ###  callibration
+
+        # Show images
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', images)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
             break
-    if not found_rgb:
-        print("The demo requires Depth camera with Color sensor")
-        exit(0)
 
-if __name__ == "__main__":
-    sprawdzenie_sprzetu()
+finally:
+
+    # Stop streaming
+    pipeline.stop()
