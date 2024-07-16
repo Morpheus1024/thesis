e66f2366d336c0d2a149de10dfb7e34a81361849
Mikołaj Galant <mikolaj.galant@gmail.com>
2024-07-16 18:36:50 +0200

    WIP 16.07.2024 wyświetlanie chmury punktów

---
+++ ply_viewer.py
@@ -0,0 +1,14 @@
+import open3d as o3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+pcd = o3d.io.read_point_cloud("cloude_of_points.ply")
+points = np.asarray(pcd.points)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(points[:,0], points[:,1], points[:,2], s=10)
+
+ax.set_title("Chmura punktów")
+
+plt.show()
