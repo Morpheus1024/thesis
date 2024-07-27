392adc97e1d6a91003792b4c05a2b12a49101b88
Mikołaj Galant <mikolaj.galant@gmail.com>
2024-07-27 16:41:20 +0200

    Dodanie zapisu masek. Czytanie o gotowych systemach semantycznych 3d

--- ply_viewer.py
+++ ply_viewer.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 pcd = o3d.io.read_point_cloud("cloude_of_points.ply")
 points = np.asarray(pcd.points)
 
+mask_path = "/maski/mask.png"
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(points[:,0], points[:,1], points[:,2], s=10)
