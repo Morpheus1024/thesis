a2aa3acd31784685e9d6700e36c92db4ff592b01
Mikołaj Galant <mikolaj.galant@gmail.com>
2024-11-05 20:16:47 +0100

    Dodanie nowego skryptu do wizualizacji chmury punktów oraz aktualizacja istniejącego skryptu do wyświetlania kolorów w chmurze punktów.

--- ply_viewer_plt.py
+++ ply_viewer_plt.py
@@ -2,12 +2,17 @@ import open3d as o3d
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Wczytanie chmury punktów
 pcd = o3d.io.read_point_cloud("semantic_map.ply")
 points = np.asarray(pcd.points)
+colors = np.asarray(pcd.colors)
 
+# Utworzenie wykresu
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(points[:,0], points[:,1], points[:,2], s=1)
+
+# Wyświetlanie chmury punktów z kolorami
+ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=colors, s=1)
 
 ax.set_title("Chmura punktów")
 
