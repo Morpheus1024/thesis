ca65063c86e4cf404c4f1c7de9ccd05f0633020d
Mikołaj Galant <mikolaj.galant@gmail.com>
2024-11-16 11:51:42 +0100

    Dodanie obsługi kamery RealSense, w tym inicjalizacja strumieni RGB i głębokości, oraz wizualizacja obrazów w czasie rzeczywistym. Dodano obsługę błędów i zakończenie działania po naciśnięciu klawisza.

--- example_2.py
+++ example_2.py
@@ -1,27 +1,61 @@
-import lib
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import pyrealsense2 as rs
+
+
 def example_2():
 
+    pipeline = rs.pipeline()
+    config = rs.config()
 
-    rs_present = lib.check_if_realsense_is_present(print_logs=False)
+    pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+    pipeline_profile = config.resolve(pipeline_wrapper)
+    device = pipeline_profile.get_device()
 
-    if rs_present:
-        
-        rs_point_cloude = lib.get_point_cloud_from_realsense()
-        print(rs_point_cloude)
+    found_rgb = False
+    for s in device.sensors:
+        if s.get_info(rs.camera_info.name) == 'RGB Camera':
+            found_rgb = True
+            break
+    if not found_rgb:
+        print("Camera not found")
+        exit(0)
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(rs_point_cloude[:,0], rs_point_cloude[:,1], rs_point_cloude[:,2], s=10)
-        ax.set_title("Point cloud")
-        plt.show()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
+    pipeline.start(config)
 
+    try:
+        while True:
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
 
+            if not depth_frame or not color_frame: continue
 
+            color_image = np.asanyarray(color_frame.get_data())
+            depth_image = np.asanyarray(depth_frame.get_data())
 
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.07), cv2.COLORMAP_JET)
 
+            if depth_colormap.shape != color_image.shape: color_image = cv2.resize(color_image, dsize=(depth_colormap.shape[1], depth_colormap.shape[0]), interpolation=cv2.INTER_AREA)
+
+            images = np.hstack((color_image, depth_colormap))
+
+            cv2.namedWindow('RealSense viewer', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q') or key == 27:
+                cv2.destroyAllWindows()
+                break
+
+    except Exception as e:
+        print(e)
+        pipeline.stop()
+
+    finally:
+        pipeline.stop()
 
 if __name__ == "__main__":
     example_2()
