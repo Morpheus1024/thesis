2a75accd1d757a2a4f014f7ac014024502e90813
Mikołaj Galant <mikolaj.galant@gmail.com>
2024-11-16 15:49:21 +0100

    Dodanie wyrównania strumieni w example_0 oraz poprawa dokumentacji w example_3. Usunięcie nieużywanych plików new.py i inz.py.

--- example_0.py
+++ example_0.py
@@ -28,12 +28,15 @@ def example_0():
 
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    align_to = rs.stream.color
+    align = rs.align(align_to)
 
     pipeline.start(config)
 
     try:
         while True:
             frames = pipeline.wait_for_frames()
+            frames = align.process(frames)
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
 
--- example_3.py
+++ example_3.py
@@ -1,7 +1,6 @@
 # Example shows how to create 3D semantic map from an loaded image
 
 import lib
-import numpy as np
 from PIL import Image
 
 
@@ -11,8 +10,8 @@ def example_3():
     image = Image.open("./example_3.png")
 
     #Segment image and get depth image
-    segmented_image, _, _  = lib.use_BEiT_semantic(image, add_legend=False)
-    depth_image, _ = lib.use_BEiT_depth(image)
+    segmented_image, _, _  = lib.use_BEiT_semantic(image, add_legend=False) # here can by used any other semantic segmentation model aviable in library
+    depth_image, _ = lib.use_BEiT_depth(image) # here can by used any other depth estimation model aviable in library
 
     # create 3D semantic map
     semantic_3d_map = lib.create_semantic_3D_map(segmented_image, depth_image, fx = 385, fy = 385, print_logs=True, save_ply=True)
